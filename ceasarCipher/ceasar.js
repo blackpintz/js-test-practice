998aaa9878268939938d397f4066e0a59e8669fd
Ubuntu <ubuntu@ip-172-31-30-105.us-east-2.compute.internal>
2020-10-07 19:19:11 +0000

    Add tests for ceasar cipher function.

--- ceasarCipher/ceasar.js
+++
@@ -1,3 +0,0 @@
-const ceaser = () => 'Hello';
-
-ceaser();
---
+++ cipher/cipher.test.js
@@ -0,0 +1,13 @@
+const cipherFunc = require('./cipher');
+
+it('checks wrapping from z to a', () => {
+  expect(cipherFunc('zoo', 1)).toBe('app');
+});
+
+it('keeps the same case', () => {
+  expect(cipherFunc('leAf', 1)).toBe('mfBg');
+});
+
+it('keeps punctuations the same', () => {
+  expect(cipherFunc('zoo!?', 1)).toBe('app!?');
+});
